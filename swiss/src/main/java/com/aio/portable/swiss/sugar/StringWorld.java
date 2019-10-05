4e933ed4286a73329669b29f7cf93db651aa1414
napple <wsrf16@outlook.com>
2019-10-05 21:48:51 +0800

    remove commons-lang3

--- swiss/src/main/java/com/aio/portable/swiss/sugar/StringWorld.java
+++ swiss/src/main/java/com/aio/portable/swiss/sugar/StringWorld.java
@@ -1,5 +1,7 @@
 package com.aio.portable.swiss.sugar;
 
+import org.springframework.util.StringUtils;
+
 public class StringWorld {
     public static String removeEnd(String str, String remove) {
         if (org.springframework.util.StringUtils.hasLength(str) && org.springframework.util.StringUtils.hasLength(remove)) {
@@ -21,8 +23,7 @@ public class StringWorld {
         return replaceEach(text, searchList, replacementList, false, 0);
     }
 
-    private static String replaceEach(
-            final String text, final String[] searchList, final String[] replacementList, final boolean repeat, final int timeToLive) {
+    private static String replaceEach(final String text, final String[] searchList, final String[] replacementList, final boolean repeat, final int timeToLive) {
 
         // mchyzer Performance note: This creates very few new objects (one major goal)
         // let me know if there are performance requests, we can create a harness to measure
@@ -146,6 +147,118 @@ public class StringWorld {
         }
 
         return replaceEach(result, searchList, replacementList, repeat, timeToLive - 1);
+    }
+
+
+
+
+
+    public static String repeat(char ch, int repeat) {
+        if (repeat <= 0) {
+            return "";
+        } else {
+            char[] buf = new char[repeat];
+
+            for(int i = repeat - 1; i >= 0; --i) {
+                buf[i] = ch;
+            }
+
+            return new String(buf);
+        }
+    }
+
+    public static String rightPad(String str, int size, char padChar) {
+        if (str == null) {
+            return null;
+        } else {
+            int pads = size - str.length();
+            if (pads <= 0) {
+                return str;
+            } else {
+                return pads > 8192 ? rightPad(str, size, String.valueOf(padChar)) : str.concat(repeat(padChar, pads));
+            }
+        }
+    }
+
+    public static String rightPad(String str, int size, String padStr) {
+        if (str == null) {
+            return null;
+        } else {
+            if (!StringUtils.hasLength(padStr)) {
+                padStr = " ";
+            }
+
+            int padLen = padStr.length();
+            int strLen = str.length();
+            int pads = size - strLen;
+            if (pads <= 0) {
+                return str;
+            } else if (padLen == 1 && pads <= 8192) {
+                return rightPad(str, size, padStr.charAt(0));
+            } else if (pads == padLen) {
+                return str.concat(padStr);
+            } else if (pads < padLen) {
+                return str.concat(padStr.substring(0, pads));
+            } else {
+                char[] padding = new char[pads];
+                char[] padChars = padStr.toCharArray();
+
+                for(int i = 0; i < pads; ++i) {
+                    padding[i] = padChars[i % padLen];
+                }
+
+                return str.concat(new String(padding));
+            }
+        }
+    }
+
+    public static String leftPad(String str, int size, char padChar) {
+        if (str == null) {
+            return null;
+        } else {
+            int pads = size - str.length();
+            if (pads <= 0) {
+                return str;
+            } else {
+                return pads > 8192 ? leftPad(str, size, String.valueOf(padChar)) : repeat(padChar, pads).concat(str);
+            }
+        }
+    }
+
+    public static String leftPad(String str, int size, String padStr) {
+        if (str == null) {
+            return null;
+        } else {
+            if (!StringUtils.hasLength(padStr)) {
+                padStr = " ";
+            }
+
+            int padLen = padStr.length();
+            int strLen = str.length();
+            int pads = size - strLen;
+            if (pads <= 0) {
+                return str;
+            } else if (padLen == 1 && pads <= 8192) {
+                return leftPad(str, size, padStr.charAt(0));
+            } else if (pads == padLen) {
+                return padStr.concat(str);
+            } else if (pads < padLen) {
+                return padStr.substring(0, pads).concat(str);
+            } else {
+                char[] padding = new char[pads];
+                char[] padChars = padStr.toCharArray();
+
+                for(int i = 0; i < pads; ++i) {
+                    padding[i] = padChars[i % padLen];
+                }
+
+                return (new String(padding)).concat(str);
+            }
+        }
+    }
+
+    public static int length(CharSequence cs) {
+        return cs == null ? 0 : cs.length();
     }
 
 }

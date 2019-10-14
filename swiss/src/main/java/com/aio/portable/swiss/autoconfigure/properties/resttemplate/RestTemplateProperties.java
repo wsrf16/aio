1f57574209aaac4b5ba266a634c09960c766ce46
yu.zhao <wsrf16@outlook.com>
2019-10-14 16:51:16 +0800

    整理并增加ByteBuddy

---
+++ swiss/src/main/java/com/aio/portable/swiss/autoconfigure/properties/resttemplate/RestTemplateProperties.java
@@ -0,0 +1,17 @@
+package com.aio.portable.swiss.autoconfigure.properties.resttemplate;
+
+//@Configuration
+public class RestTemplateProperties {
+    private Agent agent;
+
+    public Agent getAgent() {
+        return agent;
+    }
+
+    public void setAgent(Agent agent) {
+        this.agent = agent;
+    }
+
+
+
+}

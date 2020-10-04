a9ba1f339d33a591d40f5f5ecbdd253d369708d9
napple <wsrf16@outlook.com>
2020-10-04 16:29:20 +0800

    修复EventBus小bug 调整FilePO

--- spring-boot-starter-swiss/src/main/java/com/aio/portable/swiss/suite/io/NIOFiles.java
+++ spring-boot-starter-swiss/src/main/java/com/aio/portable/swiss/suite/io/NIOFiles.java
@@ -5,15 +5,16 @@ import com.aio.portable.swiss.sugar.CollectionSugar;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
+import java.nio.file.*;
 import java.nio.file.attribute.FileAttribute;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public abstract class NIOFiles {
+    public final static void write(String path, Charset charset, String content, OpenOption... options) {
+        write(Paths.get(path), charset, content, options);
+    }
+
     public final static void write(Path path, Charset charset, String content, OpenOption... options) {
         try (BufferedWriter writer = Files.newBufferedWriter(path, charset, options)) {
             writer.write(content);
@@ -21,6 +22,10 @@ public abstract class NIOFiles {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
+    }
+
+    public final static String read(String path, Charset charset) {
+        return read(Paths.get(path), charset);
     }
 
     public final static String read(Path path, Charset charset) {
@@ -44,13 +49,21 @@ public abstract class NIOFiles {
 //        }
 //    }
 
+    public final static List<Path> listFileNames(String path) {
+        return listFileNames(Paths.get(path));
+    }
+
     public final static List<Path> listFileNames(Path path) {
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)){
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
             return CollectionSugar.toList(stream.iterator()).stream().map(e -> e.getFileName()).collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
+    }
+
+    public final static void createDirectories(String path, FileAttribute<?>... attrs) {
+        createDirectories(Paths.get(path), attrs);
     }
 
     public final static void createDirectories(Path path, FileAttribute<?>... attrs) {
@@ -60,6 +73,18 @@ public abstract class NIOFiles {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
+    }
 
+    public final static void deleteIfExists(String path) {
+        deleteIfExists(Paths.get(path));
+    }
+
+    public final static void deleteIfExists(Path path) {
+        try {
+            Files.deleteIfExists(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 }
